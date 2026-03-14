cc46a133584e10f8119c05d4fa76ed3ec6166ac5
manavpethe23 <manavpethe@gmail.com>
2026-03-14 11:32:33 +0530

    commit and push project Test1

---
+++ DesignPatternTestOne/src/com/app/PatternTest.java
@@ -0,0 +1,54 @@
+package com.app;
+
+import java.util.Scanner;
+
+public class PatternTest {
+
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter number of rows: ");
+	        int n = sc.nextInt();
+
+	        // Upper half
+	        for (int i = 1; i <= n; i++) {
+
+	            // spaces
+	            for (int j = i; j < n; j++) {
+	                System.out.print(" ");
+	            }
+
+	            // stars
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+	                if (j == 1 || j == (2 * i - 1)) {
+	                    System.out.print("*");
+	                } else {
+	                    System.out.print(" ");
+	                }
+	            }
+
+	            System.out.println();
+	        }
+
+	        // Lower half
+	        for (int i = n - 1; i >= 1; i--) {
+
+	            // spaces
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+
+	            // stars
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+	                if (j == 1 || j == (2 * i - 1)) {
+	                    System.out.print("*");
+	                } else {
+	                    System.out.print(" ");
+	                }
+	            }
+
+	            System.out.println();
+	        }
+	    }
+
+}
